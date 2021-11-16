30c46005e21a145602b40f9b1fbfeff58c0864dc
Jon <jonrgull@gmail.com>
2021-11-16 21:34:43 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,11 +29,12 @@ const App = () => {
     },
   ];
 
-  return;
-  <div>
-    {/* <h2></h2> */}
-    <Expenses items={expenses} />
-  </div>;
+  return (
+    <div>
+      {/* <h2></h2> */}
+      <Expenses items={expenses} />
+    </div>
+  );
 };
 
 export default App;
